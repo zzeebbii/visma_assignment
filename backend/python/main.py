d1a25be827e747afca05e54a9cc966401b0cd2fc
Muhammad Zohaib Aslam <zohaib.aslam@nokia.com>
2023-01-05 17:15:27 +0200

    ✅ Added test cases and completed the first version
    The holiday planner is working as expected. Sunday is not included while
    the country specific holidays are excluded as required.

--- backend/python/main.py
+++ backend/python/main.py
@@ -1,10 +1,14 @@
 from datetime import date
 from holiday_planner import HolidayPlanner
 
-start_date = date(2022, 4, 1)
-end_date = date(2022, 4, 30)
+start_date = date(2022, 4, 15)
+end_date = date(2022, 5, 31)
 
 day_counter = HolidayPlanner(start_date, end_date)
-num_days = day_counter.count_days()
+num_days_finland = day_counter.count_days()
 
-print(num_days)
+print(num_days_finland)
+
+num_days_pakistan = day_counter.count_days(country="Pakistan")
+
+print(num_days_pakistan)
--- backend/python/test_holiday_planner.py
+++ backend/python/test_holiday_planner.py
@@ -12,22 +12,43 @@ class HolidayPlannerTests(unittest.TestCase):
         self.assertEqual(num_days, 24)
 
     def test_count_days_same_day(self):
-        start_date = date(2022, 4, 1)
-        end_date = date(2022, 4, 1)
+        start_date = date(year=2022, month=4, day=1)
+        end_date = date(year=2022, month=4, day=1)
         day_counter = HolidayPlanner(start_date, end_date)
         num_days = day_counter.count_days()
         self.assertEqual(num_days, 1)
 
-    def test_count_days_different_months(self):
-        start_date = date(2022, 3, 1)
-        end_date = date(2022, 4, 1)
+    def test_count_days_different_month(self):
+        start_date = date(year=2022, month=4, day=15)
+        end_date = date(year=2022, month=5, day=31)
+        day_counter = HolidayPlanner(start_date, end_date)
+        num_days = day_counter.count_days()
+        self.assertEqual(num_days, 37)
+
+    def test_count_days_different_country(self):
+        start_date = date(year=2022, month=4, day=15)
+        end_date = date(year=2022, month=5, day=31)
+        day_counter = HolidayPlanner(start_date, end_date)
+        num_days = day_counter.count_days(country="Pakistan")
+        self.assertEqual(num_days, 40)
+
+    def test_invalid_range(self):
+        start_date = date(year=2022, month=3, day=1)
+        end_date = date(year=2022, month=4, day=1)
         day_counter = HolidayPlanner(start_date, end_date)
         with self.assertRaisesRegex(ValueError, "Range is not in the same holiday period"):
             day_counter.count_days()
 
     def test_invalid_start_date(self):
-        start_date = date(2022, 5, 1)
-        end_date = date(2022, 4, 30)
+        start_date = date(year=2022, month=5, day=1)
+        end_date = date(year=2022, month=4, day=30)
         day_counter = HolidayPlanner(start_date, end_date)
         with self.assertRaisesRegex(ValueError, "chronological"):
+            day_counter.count_days()
+
+    def test_invalid_range_length(self):
+        start_date = date(year=2022, month=1, day=1)
+        end_date = date(year=2022, month=3, day=1)
+        day_counter = HolidayPlanner(start_date, end_date)
+        with self.assertRaisesRegex(ValueError, "Range should not be more than 50"):
             day_counter.count_days()
